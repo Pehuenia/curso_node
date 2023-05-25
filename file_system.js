a871d7617309a160c0005412e35ad6a801a887da
Pehuenia <msaguero@gmail.com>
2023-05-25 14:20:08 -0300

    Se agregan funciones con fs.

---
+++ file_system.js
@@ -0,0 +1,37 @@
+ const { error } = require('console');
+const fs=require('fs');
+
+//Crear un archivo txt:
+//  fs.writeFile('data1.txt','Marina - Suscribite ahora ',(error)=>{
+//     if (error) {
+//         console.log(`Error: ${error}`);
+//     }
+//  });
+
+
+//Leer un archivo txt:
+// fs.readFile('data1.txt', 'utf-8', (error, data) => {
+//     if(!error) {
+//         console.log(data);
+//     } else {
+//        console.log(`Error: {error}`);  
+//     }
+// });
+
+//Renombrar el archivo txt:
+// fs.rename('data1.txt', 'dataCanal.txt',(error) => {
+//      if (!error) {
+//         console.log("¡Renombrado!")
+//      } else {
+//         console.log(`Error: ${error}`);
+//      }
+// })
+
+//Añadir contenido al archivo txt:
+fs.appendFile('dataCanal.txt', 'Gracias por darle like a este video', (error) => {
+    if (!error) {
+        console.log("¡Contenido agregado!");
+    } else {
+        console.log(`Error: ${error}`);
+    }
+} )
