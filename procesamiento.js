7d7a5546b77d3b8fb90753c8731ee17bf7396293
naipecito <alejandria.logica@gmail.com>
2023-05-31 21:53:35 -0300

    Se agregan operaciones asíncronas

--- procesamiento.js
+++ procesamiento.js
@@ -1,10 +1,25 @@
-const { error } = require('console');
+
 const fs = require('fs');
 
-fs.readFile('dataCanal.txt','utf-8',(error,data)=>{
-    if(!error) {
-        console.log(data);
-    } else {
-        console.log(`Error: ${error}`);
-    }
-})
+// console.log("Inicio")
+// fs.readFile('dataCanal.txt', 'utf-8', (error, data) => {
+//     if(!error) {
+//         console.log(data);
+//     } else {
+//         console.log(`Error: $ {error}`);
+//     }
+// })
+// console.log("Fin");
+
+
+// console.log("Inicio")
+
+// const miData= fs.readFileSync('dataCanal.txt', 'utf-8');
+// console.log(miData)
+// console.log("Fin")
+
+console.log("Inicio")
+
+console.log(fs.readFileSync('dataCanal.txt', 'utf-8'))
+
+console.log("Fin")
